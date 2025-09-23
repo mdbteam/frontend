9347b513f82323dc5bca7d0091eb4b52d5aaf480
Bayronfu <bayr.fuentes@duocuc.cl>
2025-09-22 21:55:43 -0300

    prueba

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import PrestadorListPage from './pages/PrestadorListPage';
+import PrestadorProfilePage from './pages/PrestadorProfilePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <PrestadorListPage />,
+  },
+  {
+    path: "/perfil/:prestadorId", 
+    element: <PrestadorProfilePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
